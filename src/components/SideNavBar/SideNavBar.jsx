65958ab1780629f0387ff454cf3aa3536e763267
Ingram <netia.ingram@charter.com>
2023-05-24 20:10:02 -0600

    chore : Add event listener

--- src/components/SideNavBar/SideNavBar.jsx
+++ src/components/SideNavBar/SideNavBar.jsx
@@ -1,7 +1,18 @@
+
 export const SideNavBar = () => {
+  let buttonColor = "lightBlue"
+  const clickTestButton = () => {
+    console.log(buttonColor);
+    buttonColor === "lightBlue" ? buttonColor = "pink" : buttonColor = "lightBlue";
+  }
   return (
     <aside className="sideNavBar">
       <h3>Filters</h3>
+      <button onClick={clickTestButton} style={{
+        backgroundColor: buttonColor,
+        borderRadius: '8px',
+        marginBottom: '20px'
+      }}>Test Button</button>
     </aside>
   )
 }
