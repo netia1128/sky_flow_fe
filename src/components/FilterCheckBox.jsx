ec9c3408b208924e9f8463d658431e0942f47abd
Ingram <netia.ingram@charter.com>
2023-05-28 15:58:33 -0600

    feature (chnage-button-to-checlbox) : Did the deed

--- src/components/FilterCheckBox.jsx
+++ src/components/FilterCheckBox.jsx
@@ -4,14 +4,14 @@ export const FilterCheckBox = ({filterOrigin, origin}) => {
   const [checkboxSelected, setCheckBoxSelected] = useState(false);
 
   return (
-    <label>
-      <input type="checkbox" id={origin} value={origin} className={`filter-checkbox ${checkboxSelected ? "selected-button" : "unselected-button"}`}
-        onClick={() => {
-          setCheckBoxSelected(!checkboxSelected);
-          filterOrigin({origin}) 
-        }} 
-      />
-      {origin}
-    </label>
+    <div>
+      <input type="checkbox" id={`checkbox-${origin}`} value={origin} className={`filter-checkbox ${checkboxSelected ? "selected-button" : "unselected-button"}`}
+      onClick={() => {
+        setCheckBoxSelected(!checkboxSelected);
+            filterOrigin({origin}) 
+          }} 
+          />
+          <label htmlFor={`checkbox-${origin}`}>{origin}</label>
+    </div>
   )
 }
