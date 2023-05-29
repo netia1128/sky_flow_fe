c723b8aa5a668f4a1c82d01bf7cec642b99f9db5
Ingram <netia.ingram@charter.com>
2023-05-29 17:09:17 -0600

    feature (router) : Work on custom button

--- src/components/IngestFlightsButton.jsx
+++ src/components/IngestFlightsButton.jsx
@@ -1,8 +1,11 @@
 import addIcon from '../assets/AddIcon.png';
+import { useAxiosPost } from '../hooks/useAxiosPost';
 
 export const IngestFlightsButton = () => {
+  const ingestFlights = useAxiosPost();
+
   return (
-    <button>
+    <button onClick={ingestFlights}>
       <img src={addIcon} alt="Ingest Button"></img>
       <h2>
         Get Latest Flights
--- src/hooks/useAxiosPost.js
+++ src/hooks/useAxiosPost.js
@@ -1,38 +1,33 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
-export const useAxiosPost = (path) => {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+export function useAxiosPost (path) {
   const [isError, setIsError] = useState(false);
   const rootUrl = `http://localhost:3030`
 
-  useEffect(() => {
-    let isApiSubscribed = true;
-    const controller = new AbortController();
-    
-    const getData = async () => {
-      try {
-        const {data: fetchedData} = await axios.get(rootUrl + path, { signal: AbortSignal.timeout(5000) });
-        if (isApiSubscribed) setData(fetchedData);
-      } catch (err) {
-        console.log(err)
-        setIsError(true);
-      }
-    }
+  // useEffect(() => {
+    // let isApiSubscribed = true;
+    // const controller = new AbortController();
 
-    // Adding a timeout just to show off the loading effect 
-    setTimeout(() => {
-      getData()
-      setIsLoading(false)
-      }
-    , 1000)
-      
+    // const sendPostRequest = async () => {
+    //   try {
+    //     await axios.post(rootUrl + path, { signal: AbortSignal.timeout(5000) });
+    //   } catch (err) {
+    //     console.log(err)
+    //     setIsError(true);
+    //   }
+    // }
+
+    // sendPostRequest()
+    
+    // return () => {
+    //   isApiSubscribed = false;
+    //   controller.abort();
+    //   }
+    // }, [path]
+    // )
+
     return () => {
-      isApiSubscribed = false;
-      controller.abort();
-      }
-    }, [path]
-    )
-    return { data, isLoading, isError}
+      console.log('ehyyy');
+    }
 }
